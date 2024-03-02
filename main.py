8b7d94285fafbd89c627f2f8d0fd96d0717f91f7
BML-M <161972511+BML-M@users.noreply.github.com>
2024-03-02 22:21:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+def test():
+    a = 1
+    b = 2
+    print(a, b)
+
+
+test()
+
+
+def test_2(a, b, c):
+    print(a, b, c)
+
+
+a_1 = '1-'
+b_2 = '5'
+c_1 = 'Я только учусь .)'
+test_2(a_1, b_2, c_1)
