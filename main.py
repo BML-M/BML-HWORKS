ecdf44351fbc1b2c08370a9afada548d1c4b53b3
BML-M <161972511+BML-M@users.noreply.github.com>
2024-03-10 14:58:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-def test_Muxa(*args):
-    print('test_Muxa')
-    print('тип args:', type(args))
-    print(args)
-    for i, arg in enumerate(args):
-        print(i, arg)
-
-
-test_Muxa('привет', 'я думаю', ' если я все правильно понял', 2, 2444, 5.6, 3.5, 188)
-
-
-def test_Muxa_2(n):
-    if n == 1:
-        return 1
-    test_Muxa_3 = test_Muxa_2(n=n - 1)
-    return n * test_Muxa_3
-
-
-print(test_Muxa_2(45))
+import module_1
+print(__name__)
+print(dir(module_1))
+print(dir(module_1.print_params))
+print(help(module_1))
+module_1.print_params('Привет')
+module_1.print_param_2('Good luck')
